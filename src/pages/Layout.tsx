f33bc25e42e3e1344996925538369ec90306f390
amandavalencia <amandaavalencia@hotmail.com>
2024-11-26 16:30:00 +0100

    first commit

---
+++ src/pages/Layout.tsx
@@ -0,0 +1,14 @@
+export const AbLayoutout =()=>{
+
+    return(
+        <nav>
+            <li>Home</li>
+            <li>Training Programs</li>
+            <li>My training Programs</li>
+            <li>Who is Karlsson</li>
+        </nav>
+        <main>
+            
+        </main>
+    )
+}
---
+++ src/pages/MyPrograms.tsx
@@ -0,0 +1,6 @@
+export const MyPrograms =()=>{
+
+    return(
+        <h1>My training Programs</h1>
+    )
+}
