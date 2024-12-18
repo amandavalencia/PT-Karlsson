9087828f9138dc8f0b6dd8d24c755c7536214959
amandavalencia <amandaavalencia@hotmail.com>
2024-12-18 21:22:51 +0100

    update layout

---
+++ src/components/About.tsx
@@ -0,0 +1,12 @@
+export const About = () => {
+  return (
+    <>
+      <div
+        id="about"
+        className="flex flex-col items-center justify-center h-screen p-4 gap-6 md:flex-row md:gap-8"
+      >
+        <h1>About </h1>
+      </div>
+    </>
+  );
+};
---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+export const Footer = () => {
+  return (
+    <>
+      <div className="flex flex-col items-center justify-center h-2/5 p-4 bg-greyBakground">
+        <h1>Footer</h1>;
+      </div>
+    </>
+  );
+};
